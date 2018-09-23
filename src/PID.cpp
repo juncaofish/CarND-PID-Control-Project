2969dc4935183175b48816a6d1b913c5348c207f
曹军 <juncaofish@sina.com>
2018-09-24 07:29:06 +0800

    submit missing file for pid control

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,24 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double _Kp, double _Ki, double _Kd) {
+    Kp = _Kp;
+    Ki = _Ki;
+    Kd = _Kd;
+    i_error = 0;
+    p_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    i_error += cte;
+    d_error = cte - p_error;
+    p_error = cte;
 }
 
 double PID::TotalError() {
+    return -(Kp * p_error + Ki * i_error + Kd * d_error);
 }
 
